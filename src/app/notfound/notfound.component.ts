57947f73491dc5026f0082986a6338edd21ea5f7
GuruRaghavendra <hguru001@gmail.com>
2022-03-07 16:05:20 +0530

    updated not found component

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
+
+  movetodash()
+	{
+		
+		this.router.navigate(['/dashboard/e-commerce']);
+	}
+  logout()
+	{
+		localStorage.clear();
+		this.router.navigate(['/login']);
+
+	}
 
 }
